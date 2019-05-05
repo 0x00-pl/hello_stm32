312f03432ae528ff1ec2f2bdf6331d5839b9eeea
0x00-pl <0x00.pl@gmail.com>
2019-05-05 14:04:54 +0100

    update

--- Src/my_code.c
+++ Src/my_code.c
@@ -1,22 +1,26 @@
 #include "my_code.h"
 
-uint32_t get_temperature(ADC_HandleTypeDef* hadc){
+uint32_t get_temperature_x100(ADC_HandleTypeDef* hadc){
     HAL_ADC_Start(hadc);
 
     HAL_ADC_PollForConversion(hadc, 100);
 
-    uint32_t value = HAL_ADC_GetValue(hadc);
+    uint32_t sensorData = HAL_ADC_GetValue(hadc);
 
     HAL_ADC_Stop(hadc);
-	return value;
+
+    //	  float V25 = 1.43f;
+    //	  float AVGSLOPE = 4.3f;
+    //	  float temperature = ((1.43f * 1000 - sensorData * 0.8) / 4.3f) + 25;
+	return (1430000 - (sensorData * 800)) / 43 + 2500;
 }
 
 
 void loop(ADC_HandleTypeDef* hadc1, UART_HandleTypeDef* huart1){
 	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-	  uint32_t val = get_temperature(hadc1);
+	  uint32_t temperature_x100 = get_temperature_x100(hadc1);
 	  char buff[1024];
-	  sprintf(buff, "hello: %ld\n", val);
+	  sprintf(buff, "temperature: %ld.%02ldC\n", temperature_x100/100, temperature_x100%100);
 	  HAL_UART_Transmit(huart1, (uint8_t*)buff, strlen(buff), 1000);
 	  HAL_Delay(1000);
 }
--- Src/my_code.h
+++ Src/my_code.h
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <string.h>
 
-uint32_t get_temperature(ADC_HandleTypeDef* hadc);
+uint32_t get_temperature_x100(ADC_HandleTypeDef* hadc);
 
 
 void loop(ADC_HandleTypeDef* hadc1, UART_HandleTypeDef* huart1);
